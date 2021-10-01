2734ef5d36cf6dc3da971dccb7b2fa418eee93e1
JasonLee <xiaotian027@hotmail.com>
2021-10-01 16:13:10 +0800

    demo js

---
+++ app.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.listen(8080, ()=>{
+    console.log("Listening on port 8080");
+});
